3ed4a5f5924fcfa8d291ba97bce3ea4310df7a38
Harvey Yang <yanghanji1@gmail.com>
2024-07-16 11:22:29 +0800

    重构代码

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
+// 创建一个函数随机返回“rock”、“paper”或“scissors”
 function getComputerChoice() {
-  let num = Math.floor(Math.random() * 3);
-  if (num === 0) {
-    return "Rock";
-  } else if (num == 1) {
-    return "Scissors";
-  } else {
-    return "Paper";
-  }
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
 }
 
+// 创建一个函数获取用户输入并返回
 function getHumanChoice() {
-  let human = prompt("What do you want?", "Rock? Scissors? Paper?");
+  let human = prompt("What do you want? Rock, Paper or Scissors?");
   return human;
 }
 
+// 声明玩家和电脑的得分变量
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame() {
-  function playRound(humanChoice, computerChoice) {
-    let human = humanChoice().toUpperCase();
-    let compute = computerChoice().toUpperCase();
-    if (human === compute) {
-      console.log("平局");
-    } else if (human === "ROCK" && compute === "SCISSORS") {
-      console.log("你赢了，石头战胜剪刀");
-      humanScore++;
-    } else if (human === "ROCK" && compute === "PAPER") {
-      console.log("你输了，布战胜石头");
-      computerScore++;
-    } else if (human === "SCISSORS" && compute === "ROCK") {
-      console.log("你输了，石头战胜剪刀");
-      computerScore++;
-    } else if (human === "SCISSORS" && compute === "PAPER") {
-      console.log("你赢了，剪刀战胜布");
-      humanScore++;
-    } else if (human === "PAPER" && compute === "ROCK") {
-      console.log("你赢了，布战胜石头");
-      humanScore++;
-    } else if (human === "PAPER" && compute === "SCISSORS") {
-      console.log("你输了，剪刀战胜布");
-      computerScore++;
-    } else {
-      console.log("你输入的什么b玩意儿");
-      console.log("只能输入上面那3个");
-    }
+// 创建一个函数进行一轮游戏
+function playRound(humanChoice, computerChoice) {
+  let human = humanChoice.toLowerCase();
+  let compute = computerChoice.toLowerCase();
+
+  if (human === compute) {
+    console.log("平局");
+  } else if (
+    (human === "rock" && compute === "scissors") ||
+    (human === "scissors" && compute === "paper") ||
+    (human === "paper" && compute === "rock")
+  ) {
+    console.log(`你赢了，${humanChoice}战胜${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`你输了，${computerChoice}战胜${humanChoice}`);
+    computerScore++;
   }
-
-  playRound(getHumanChoice, getComputerChoice);
-  playRound(getHumanChoice, getComputerChoice);
-  playRound(getHumanChoice, getComputerChoice);
-  playRound(getHumanChoice, getComputerChoice);
-  playRound(getHumanChoice, getComputerChoice);
-
-  return humanScore > computerScore
-    ? console.log(`最终你赢了，你有${humanScore}分，电脑有${computerScore}分`)
-    : console.log(`最终你输了，你有${humanScore}分，电脑有${computerScore}分`);
 }
 
+// 创建一个函数进行5轮游戏
+function playGame() {
+  // 进行5轮游戏
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+
+  // 最终宣布胜者
+  if (humanScore > computerScore) {
+    console.log(`最终你赢了，你有${humanScore}分，电脑有${computerScore}分`);
+  } else if (humanScore < computerScore) {
+    console.log(`最终你输了，你有${humanScore}分，电脑有${computerScore}分`);
+  } else {
+    console.log(`最终平局，你有${humanScore}分，电脑有${computerScore}分`);
+  }
+}
+
+// 开始游戏
 playGame();
